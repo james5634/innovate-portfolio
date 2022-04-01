8d0d0ec5af8b5cd5f5b68edcb4226c4f6b5ad66e
James Burgess <jamesburgess111@hotmail.co.uk>
2022-04-01 10:29:59 +0100

    inital fullstack commit

---
+++ fullstack_project/app.py
@@ -0,0 +1,49 @@
+##################################
+#! FLASK APPLICATION
+##################################
+#extensions that alllow specific class and functions
+from flask import Flask, Blueprint, render_template, url_for, redirect
+
+#a 'bluerint' if the website structure
+views = Blueprint(__name__, "views")
+
+#the wesite is defined as a flask app and the url prefix is set to '/'.
+app =Flask(__name__)
+app.register_blueprint(views, url_prefix = "/")
+
+##################################
+#! APPLICATION PAGES
+##################################
+
+#this is how the homepage is accessed.
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/james")
+def james_page():
+    return render_template("james.html", favourite_thing = "Pizza")
+
+@app.route("/katy")
+def katy_page():
+    return render_template("katy.html", favourite_thing = "Fish")
+
+##################################
+
+@app.route("/home")
+def home_redirect():
+    return redirect(url_for("home"))
+
+##################################
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"),404
+
+##################################
+
+#debugging is activated and the prject is set to hosted on port 8000 (debigging should only be used in testing)
+if __name__ == "__main__":
+    app.run(debug = True, port =8000)
+
+##################################
